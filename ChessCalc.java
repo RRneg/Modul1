a65ddd47874e15ef37b356cfd598a568f154c367
RRneg <71498446+RRneg@users.noreply.github.com>
2020-10-12 16:03:48 +0300

    Add files via upload

---
+++ ChessCalc.java
@@ -0,0 +1,17 @@
+public class ChessCalc {
+    int[] move;
+
+    public ChessCalc(int[] move) {
+        this.move = move;
+    }
+
+    public void chessCalc(int[] move) {
+        int gor = (int) Math.abs(move[0] - move[2]);
+        int vert = (int) Math.abs(move[1] - move[3]);
+        if ((gor == 1 && vert == 2) || (gor == 2 && vert == 1)) {
+            System.out.println("So you can move a knight");
+        } else {
+            System.out.println("So you can't move a knight");
+        }
+    }
+}
